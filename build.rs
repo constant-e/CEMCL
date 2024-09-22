8e4ecb24b811a60a7edd87a31d4002ef54a6bc53
constant-e <FMN_TZX@outlook.com>
2024-09-22 18:20:52 +0800

    chore: force slint to use qt backend

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
 fn main() {
-    slint_build::compile("res/ui/appwindow.slint").unwrap();
+    let config = slint_build::CompilerConfiguration::new()
+        .with_style("qt".into());
+    slint_build::compile_with_config("res/ui/appwindow.slint", config).unwrap();
 }
