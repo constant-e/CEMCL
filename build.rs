48cf96c05c5b1e14f611177dafaeede1c4ac0033
constant-e <FMN_TZX@outlook.com>
2025-01-01 12:07:35 +0800

    chore: not use qt by default

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 fn main() {
     let config = slint_build::CompilerConfiguration::new()
-        .with_bundled_translations("res/translation")
-        .with_style("qt".into());
+        .with_bundled_translations("res/translation");
     slint_build::compile_with_config("res/ui/appwindow.slint", config).unwrap();
 }
