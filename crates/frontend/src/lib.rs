c0ba51285abb8d66346e80318497fbf6efe627fe
constant-e <fmn_tzx@outlook.com>
2026-08-07 09:34:35 +0800

    translations: update translations

--- crates/frontend/src/lib.rs
+++ crates/frontend/src/lib.rs
@@ -2,11 +2,12 @@ pub mod account;
 mod app_window;
 pub mod game;
 pub mod home;
-pub mod msg_box;
+mod msg_box;
 mod settings;
 mod ui;
 
 pub use app_window::AppWindow;
 pub use app_window::UICommand;
 pub use app_window::UIUpdate;
+pub use msg_box::MsgID;
 pub use settings::{Config, ConfigDL, ConfigGeneral, ConfigMC};
